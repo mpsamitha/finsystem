fbf5a35300149b8a0ca8fa73b6ac0994cea1b8b7
Samitha-PC\Samitha <mpsamitha@yahoo.com>
2018-03-12 18:42:22 +0530

    second commit 12/3/2018

---
+++ src/main/java/com/sam/finsystem/dao/EmployeeDao.java
@@ -0,0 +1,19 @@
+package com.sam.finsystem.dao;
+
+import java.util.List;
+
+import com.sam.finsystem.model.Employee;
+ 
+public interface EmployeeDao {
+ 
+    Employee findById(int id);
+ 
+    void saveEmployee(Employee employee);
+     
+    void deleteEmployeeBySsn(String ssn);
+     
+    List<Employee> findAllEmployees();
+ 
+    Employee findEmployeeBySsn(String ssn);
+ 
+}
